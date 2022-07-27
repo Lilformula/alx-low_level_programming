cb50b9b677fa21e1c63d19d383557766bd797b1d
Ben-Gurion Zanus <106863674+Lilformula@users.noreply.github.com>
2022-07-27 13:54:01 +0000

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,40 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - prints minimum number of coins to make change for an amount of money.
+ * main - prints all arguments it recieves
  * @argc: number of arguments passed to the function
  * @argv: argument vector of pointers to strings
  *
- * Return: 0 if no errors, else 1
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
-	int a, n = 0, i, t;
-	int c[5] = {25, 10, 5, 2, 1};
+	int i;
 
-	if (argc != 2)
+	for (i = 0; i < argc; i++)
 	{
-		puts("Error");
-		return (1);
+		printf("%s\n", argv[i]);
 	}
-	a = atoi(argv[1]);
-	if (a <= 0)
-	{
-		puts("0");
-		return (1);
-	}
-	else
-	{
-		for (i = 0; i < 5; i++)
-		{
-			t = a / c[i];
-			a -= t * c[i];
-			n += t;
-			if (a == 0)
-				break;
-		}
-	}
-	printf("%d\n", n);
 	return (0);
 }
